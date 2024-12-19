4a22af6565d3b8dbf2e30faeb2df743dd2260812
AhmedELNajjar-dev <ahmed.mamdouh.517@h-eng.helwan.edu.eg>
2024-12-19 18:51:47 +0200

    adding_pics

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,16 +3,16 @@ import { Product } from '../types';
 export const products: Product[] = [
   {
     id: '1',
-    name: 'Classic Journey Hoodie',
-    description: 'Our signature hoodie with the Journey explorer logo',
+    name: 'Stellar Art Hoodie',
+    description: 'Immerse yourself in cosmic elegance—a timeless blend of classical art and celestial charm.',
     price: 400,
     images: [
-      '/images/products/hoodie1-front.jpg',
-      '/images/products/hoodie1-back.jpg'
+      
+      '/images/products/Art/1-art.png'
     ],
     sizes: ['L', 'XL', 'XXL'],
     gender: 'unisex',
-    color: 'offwhite',
+    color: 'black',
     stock: {
       'L': 20,
       'XL': 8,
@@ -21,12 +21,31 @@ export const products: Product[] = [
   },
   {
     id: '2',
-    name: 'Urban Explorer Hoodie',
-    description: 'Comfortable urban style hoodie perfect for city adventures',
+    name: 'Eternal Rose Hoodie',
+    description: 'Elegance in full bloom. Celebrate the timeless beauty of roses with this artfully designed hoodie, perfect for any casual look.',
     price: 400,
     images: [
-      '/images/products/hoodie2-front.jpg',
-      '/images/products/hoodie2-back.jpg'
+      '/images/products/Rose/2-rose.png',
+      
+    ],
+    sizes: ['L', 'XL', 'XXL'],
+    gender: 'unisex',
+    color: 'black',
+    stock: {
+      'L': 15,
+      'XL': 10,
+      'XXL': 5
+    }
+  },
+  {
+    id: '3',
+    name: 'Beacon of Hope Hoodie',
+    description: 'Carry the light of hope wherever you go with this powerful design—a symbol of strength and perseverance.',
+    price: 400,
+    images: [
+      // '/images/products/hoodie2-front.jpg',
+      '/images/products/Hope/3-Hope.png'
+      // '/images/products/hoodie2-back.jpg'
     ],
     sizes: ['L', 'XL', 'XXL'],
     gender: 'unisex',
@@ -38,21 +57,22 @@ export const products: Product[] = [
     }
   },
   {
-    id: '3',
-    name: 'Desert Journey Hoodie',
-    description: 'Elegant beige hoodie perfect for casual wear',
+    id: '4',
+    name: 'Wild Moon Hoodie',
+    description: 'Unleash your spirit with the Wild Moon Hoodie,where celestial vibes meet untamed adventure.',
     price: 400,
     images: [
-      'https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?auto=format&fit=crop&w=800&q=80',
-      'https://images.unsplash.com/photo-1620799140188-3b2a02fd9a77?auto=format&fit=crop&w=800&q=80'
+      // '/images/products/hoodie2-front.jpg',
+      '/images/products/staywild/4-stay_wild.png'
+      // '/images/products/hoodie2-back.jpg'
     ],
     sizes: ['L', 'XL', 'XXL'],
     gender: 'unisex',
-    color: 'beige',
+    color: 'black',
     stock: {
-      'L': 15,
-      'XL': 10,
-      'XXL': 5
+      'L': 0,
+      'XL': 0,
+      'XXL': 0
     }
   }
 ];
